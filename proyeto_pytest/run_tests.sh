17ba890cccadecb81c505ed2d0d80aed8d386d40
ficou <ficoupay@gmail.com>
2026-01-15 21:50:22 +0100

    cambiando cosas

--- proyeto_pytest/run_tests.sh
+++ proyeto_pytest/run_tests.sh
@@ -1,17 +1,36 @@
 #!/bin/bash
-echo "Running tests with pytest..."
+
+echo "Iniciando ejecución de pruebas en Jenkins..."
+
+# Verificar si el entorno virtual existe
 if [ ! -d "venv" ]; then
-    echo "Creating virtual environment..."
+    echo "Entorno virtual no encontrado. Creándolo..."
     python3 -m venv venv
 fi
-source venv/bin/activate
 
-echo"installing dependencies..."
+# Activar el entorno virtual
+if [ -f "venv/bin/activate" ]; then
+    source venv/bin/activate
+elif [ -f "venv/Scripts/activate" ]; then
+    source venv/Scripts/activate   # Para Windows
+else
+    echo "Error: No se pudo activar el entorno virtual."
+    exit 1
+fi
 
+# Instalar dependencias
+echo "Instalando dependencias..."
 pip install --upgrade pip
 pip install -r requirements.txt
 
-echo "Starting tests..."
-pytest tests/ --junitxml=reports/test-results.xml --html=reports/test_results.html --self-contained-html
+# Crear carpeta de reportes si no existe
+mkdir -p reports
 
-echo "Tests completed. Reports generated in the reports"
+# Ejecutar las pruebas con pytest
+echo "Ejecutando pruebas con pytest..."
+venv/bin/python -m pytest tests/ \
+    --junitxml=reports/test-results.xml \
+    --html=reports/report.html \
+    --self-contained-html
+
+echo "Pruebas finalizadas. Reportes en reports/"
